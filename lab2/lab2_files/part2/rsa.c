b4888480bf8b67a754ecf801bb81cc9e31f66df3
Paul J. Han <pauljhan@utexas.edu>
2026-02-11 03:11:05 -0600

    Lab 2 Part 2: pad plaintext for RSA

--- lab2/lab2_files/part2/rsa.c
+++ lab2/lab2_files/part2/rsa.c
@@ -3,47 +3,78 @@
 #include <stdint.h>
 #include <math.h>
 #include <stdint.h>
+#include <string.h>
 
 typedef struct uint128 {
   uint64_t hi;
   uint64_t lo;
 } uint128;
 
+void pad_pkcs15(char * padded_plaintext, const char plaintext[]);
+
+
 int main() {
-/* Private-Key: (128 bit)                                                                                                                                         */
-/* modulus: */
-/*    00:e0:37:d3:5a:8b:16:0e:b7:f1:19:19:bf:ef:44: */
-/*    09:17 */
-/* publicExponent: 65537 (0x10001) */
-/* privateExponent: */
-/*    00:ca:b1:0c:ca:a4:43:7b:67:11:c9:77:a2:77:fe: */
-/*    00:a1 */
-/* prime1: 18125493163625818823 (0xfb8aafffd4b02ac7) */
-/* prime2: 16442969659062640433 (0xe43129c94cf45f31) */
-/* exponent1: 5189261458857000451 (0x4803f5cd8dcbfe03) */
-/* exponent2: 12850891953204883393 (0xb2578a24fdb3efc1) */
-/* coefficient: 10155582946292377246 (0x8cefe0e210c5a69e) */
+	/* Private-Key: (128 bit)                                                                                                                                         */
+	/* modulus: */
+	/*    00:e0:37:d3:5a:8b:16:0e:b7:f1:19:19:bf:ef:44: */
+	/*    09:17 */
+	/* publicExponent: 65537 (0x10001) */
+	/* privateExponent: */
+	/*    00:ca:b1:0c:ca:a4:43:7b:67:11:c9:77:a2:77:fe: */
+	/*    00:a1 */
+	/* prime1: 18125493163625818823 (0xfb8aafffd4b02ac7) */
+	/* prime2: 16442969659062640433 (0xe43129c94cf45f31) */
+	/* exponent1: 5189261458857000451 (0x4803f5cd8dcbfe03) */
+	/* exponent2: 12850891953204883393 (0xb2578a24fdb3efc1) */
+	/* coefficient: 10155582946292377246 (0x8cefe0e210c5a69e) */
 
-    //DO NOT MODIFY
-    uint128 modulus = {0xe037d35a8b160eb7LL,  0xf11919bfef440917LL};
-    uint128 privateExp = {0x00cab10ccaa4437b67LL,  0x11c977a277fe00a1LL};
-    uint64_t pubExp = 65537;
-    const char plaintext[] = "Hello !";
-    uint128 ciphertext;
-    uint128 decrypted;
-    //END DO NOT MODIFY
+	//DO NOT MODIFY
+	uint128 modulus = {0xe037d35a8b160eb7LL,  0xf11919bfef440917LL};
+	uint128 privateExp = {0x00cab10ccaa4437b67LL,  0x11c977a277fe00a1LL};
+	uint64_t pubExp = 65537;
+	const char plaintext[] = "Hello !";
+	uint128 ciphertext;
+	uint128 decrypted;
+	//END DO NOT MODIFY
 
-    /* YOUR CODE HERE: Implement RSA encryption, write the encrypted output of plaintext to ciphertext */
+	/* YOUR CODE HERE: Implement RSA encryption, write the encrypted output of plaintext to ciphertext */
+	/*
+	* We can encrypt using the public key then decrypt using the private key
+	* Encryption can occur using these steps
+	* 1. Compute public key which consists of the modulus n and the public exponent e. Fortunately, both nunmbers are given to us
+	* 2. Compute the integer representation of the message using the PKCS 1.5 padding scheme
+	*/
+	#define NUM_PADDED_BYTES 8
+	int len = strlen(plaintext) + 1;
+	char * padded_plaintext = (char *)malloc(sizeof(char) * (len + NUM_PADDED_BYTES));
+	pad_pkcs15(padded_plaintext, plaintext);
+	
+	len = strlen(padded_plaintext) + 1;
+	for(int i = 0; i < len; i++){
+		printf("%x", padded_plaintext);
+	}
+	printf("\n");
 
-    //DO NOT MODIFY
-    char *encrypted_text = (char*)&ciphertext;
-    printf("encrypted=%s\n", encrypted_text);
-    //END DO NOT MODIFY
+	//DO NOT MODIFY
+	char *encrypted_text = (char*)&ciphertext;
+	printf("encrypted=%s\n", encrypted_text);
+	//END DO NOT MODIFY
 
-    /* YOUR CODE HERE: Implement RSA decryption, write the decrypted output of ciphertext to decrypted */
+	/* YOUR CODE HERE: Implement RSA decryption, write the decrypted output of ciphertext to decrypted */
 
-    //DO NOT MODIFY
-    char *decrypted_text = (char*)&decrypted;
-    printf("decrypted=%s\n", decrypted_text);
-    assert(strcmp(plaintext, decrypted_text) == 0);
+	//DO NOT MODIFY
+	char *decrypted_text = (char*)&decrypted;
+	printf("decrypted=%s\n", decrypted_text);
+	assert(strcmp(plaintext, decrypted_text) == 0);
+}
+
+void pad_pkcs15(char * padded_plaintext, const char plaintext[]){
+	int len = strlen(plaintext);
+	for(int i = 0; i <= len; i++){
+		padded_plaintext[i] = plaintext[i];
+	}
+
+	padded_plaintext[len+1] = 0x00;
+	padded_plaintext[len+2] = 0x02;
+	padded_plaintext[len+3] = 0x00;
 }
