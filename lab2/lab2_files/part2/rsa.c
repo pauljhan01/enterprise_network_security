b0561986660139b64d72fb92ec0cf7bb91b74e1b
Paul J. Han <pauljhan@utexas.edu>
2026-02-12 05:17:04 -0600

    Lab 2 Part 2: modular exponentation, but uint128 mod has issues

--- lab2/lab2_files/part2/rsa.c
+++ lab2/lab2_files/part2/rsa.c
@@ -16,10 +16,11 @@ typedef struct message_text{
 	uint32_t len;
 }msg_txt_t;
 
-void pad_pkcs15(char * padded_plaintext, const char plaintext[]);
+void write_arr_to_uint128(uint128 * message, const char plaintext[]);
 
-void pack_char_arr_uint128(msg_txt_t * message_txt, uint128 * message);
+void modular_power_public(uint128 * ciphertext, uint128 * message, uint128 * modulus, uint64_t exp);
 
+void modular_power_private(uint128 * ciphertext, uint128 * message, uint128 * modulus, uint64_t exp);
 
 int main() {
 	/* Private-Key: (128 bit)                                                                                                                                         */
@@ -52,20 +53,24 @@ int main() {
 	* 1. Compute public key which consists of the modulus n and the public exponent e. Fortunately, both nunmbers are given to us
 	* 2. Compute the integer representation of the message using the PKCS 1.5 padding scheme
 	*/
-	#define NUM_PADDED_BYTES 8
-	int len = NUM_PADDED_BYTES + strlen(plaintext) + 1;
-	
-	msg_txt_t * message_txt = (msg_txt_t *)calloc(1, sizeof(msg_txt_t));
-	message_txt->string = (char *)calloc(len, sizeof(char));
-	message_txt->len = len;
-	pad_pkcs15(message_txt->string, plaintext);
-	
-	uint128 * message = (uint128 *)calloc(1, sizeof(uint128));
-	pack_char_arr_uint128(message_txt, message);	
+	ciphertext.lo = 0x0;
+	ciphertext.hi = 0x0;
+	uint128 message;
+	message.lo = 0x0;
+	message.hi = 0x0;
+	write_arr_to_uint128(&message, plaintext);
 
-	free(message_txt->string);
-	free(message_txt);
-	
+	message.lo = 4;
+	message.hi = 0;
+
+	modulus.lo = 497;
+	modulus.hi = 0;
+
+	pubExp = 13;
+
+	modular_power_public(&ciphertext, &message, &modulus, pubExp);
+
+	printf("%lu\n", ciphertext.lo);	
 	//DO NOT MODIFY
 	char *encrypted_text = (char*)&ciphertext;
 	printf("encrypted=%s\n", encrypted_text);
@@ -79,34 +84,182 @@ int main() {
 	assert(strcmp(plaintext, decrypted_text) == 0);
 }
 
-void pack_char_arr_uint128(msg_txt_t * message_txt, uint128 * message){
-	uint32_t shifts = 0;
-	for(int i = 0; i < message_txt->len; i++){
-		if(i * 8 >= 64){
-			message->hi = message->hi | (message_txt->string[i] << (i*8));
-		}else{
-			message->lo = message->lo | (message_txt->string[i] << (i*8));
+int compare(uint128 * a, uint128 * b){
+	if(a->hi != b->hi){
+		return (a->hi > b->hi) ? 1 : -1;
+	}
+
+	if(a->lo != b->lo){
+		return (a->lo > b->lo) ? 1 : -1;
+	}
+
+	return 0;
+}
+
+void subtract(uint128 * output, uint128 * a, uint128 * b){
+	output->lo = a->lo - b->lo;
+	output->hi = a->hi - b->hi;
+
+	if (a->lo < b->lo){
+		output->hi--;
+	}
+}
+
+void left_shift(uint128 * output, uint128 * a){
+	output->hi = (a->hi << 1) | (a->lo >> 63);
+	output->lo = a->lo << 1;
+}
+
+uint64_t uint128_to_64_mod(uint128 * dividend, uint64_t divisor){
+	if(divisor == 0){
+		return 0;
+	}
+
+	if(dividend->hi == 0){
+		return dividend->lo % divisor;
+	}
+
+	uint64_t remainder = 0;
+	remainder = dividend->hi % divisor;
+
+	for(int i = 63; i >= 0; i--){
+		remainder = (remainder << 1) | ((dividend->lo >> i) & 1);
+		if(remainder >= divisor){
+			remainder -= divisor;
 		}
-		printf("%x", message_txt->string[i]);
 	}
-	printf("\n");
+
+	return remainder;
 }
 
-void pad_pkcs15(char * padded_plaintext, const char plaintext[]){
-	int len = strlen(plaintext);
-	padded_plaintext[0] = 0x00;
-	padded_plaintext[1] = 0x02;
-
-	for(int i = 2; i < NUM_PADDED_BYTES - 1; i++){
-		padded_plaintext[i] = 0x03;
+void uint128_to_uint128_mod(uint128 * output, uint128 * dividend, uint128 * divisor){
+	if (divisor->hi == 0 && divisor->lo == 0){
+		output->hi = 0;
+		output->lo = 0;
+		return;
 	}
 
-	padded_plaintext[NUM_PADDED_BYTES - 1] = 0x00;
-
-	//include NULL terminator
-	for(int i = 0; i < len + 1; i++){
-		padded_plaintext[i + NUM_PADDED_BYTES] = plaintext[i];
+	if(compare(dividend, divisor) < 0){
+		*output = *dividend;
+		return;
 	}
-	
-	
+
+	uint128 remainder = *dividend;
+
+	int shift = 0;
+	uint128 shifted_divisor;
+	shifted_divisor.hi = divisor->hi;
+	shifted_divisor.lo = divisor->lo;
+	uint128 temp;
+
+	while(1){
+		left_shift(&temp, &shifted_divisor);
+		if(compare(&temp, dividend) > 0){
+			break;
+		}
+
+		shifted_divisor = temp;
+		shift++;
+	}
+
+	for(int i = shift; i >= 0; i++){
+		if(compare(&remainder, &shifted_divisor) >= 0){
+			uint128 remainder_temp;
+			subtract(&remainder, &remainder, &shifted_divisor);	
+		}
+
+		shifted_divisor.lo = (shifted_divisor.lo >> 1) | (shifted_divisor.hi << 63);
+		shifted_divisor.hi = (shifted_divisor.hi >> 1);
+	}
+
+	output->hi = remainder.hi;
+	output->lo = remainder.lo;	
 }
+
+void add(uint128 * output, const uint128 * a, const uint128 * b){
+	output->lo = a->lo + b->lo;
+	output->hi = a->hi + b->hi;
+
+	if(output->lo < a->lo){
+		output->hi++;
+	}
+}
+
+void uint64_to_uint128_mul(uint128 * output, uint64_t a, uint64_t b){
+	uint64_t a_lo = a & 0xFFFFFFFFULL;
+    uint64_t a_hi = a >> 32;
+    uint64_t b_lo = b & 0xFFFFFFFFULL;
+    uint64_t b_hi = b >> 32;
+    
+    uint64_t p0 = a_lo * b_lo;
+    uint64_t p1 = a_lo * b_hi;
+    uint64_t p2 = a_hi * b_lo;
+    uint64_t p3 = a_hi * b_hi;
+    
+    uint64_t middle = p1 + (p0 >> 32);
+    middle += p2;
+    
+    output->lo = (middle << 32) | (p0 & 0xFFFFFFFFULL);
+    output->hi = p3 + (middle >> 32);
+    
+    if (middle < p1) {
+        output->hi += 0x100000000ULL;
+    }	
+}
+
+void mul(uint128 * output, const uint128 * a, const uint128 * b){
+	uint128 low_product;
+	uint64_to_uint128_mul(&low_product, a->lo, b->lo);
+
+	uint64_t mid1 = a->hi * b->lo;
+	uint64_t mid2 = a->lo * b->hi;
+
+	output->lo = low_product.lo;
+	output->hi = low_product.hi + mid1 + mid2;
+}
+
+void modular_power_public(uint128 * ciphertext, uint128 * message, uint128 * modulus, uint64_t exp){
+		if((modulus->hi + modulus->lo) == 1){
+			ciphertext->hi = 0;
+			ciphertext->lo = 0;
+			return;
+		}
+
+		ciphertext->lo = 1;
+		ciphertext->hi = 0;
+
+		uint128 temp;
+
+		uint128 base = *message;
+		uint128_to_uint128_mod(&base, &base, modulus);
+
+		while (exp > 0){
+			if(exp % 2 == 1){
+				temp = *ciphertext;
+				mul(ciphertext, &temp, &base);
+
+				temp = *ciphertext;
+				uint128_to_uint128_mod(ciphertext, &temp, modulus);
+			}
+			exp = exp >> 1;
+
+			mul(&base, &base, &base);
+			uint128_to_uint128_mod(&base, &base, modulus);
+		}
+}
+
+void write_arr_to_uint128(uint128 * message, const char plaintext[]){
+	int len = strlen(plaintext) + 1;
+
+	for(int i = 0; i < len/2; i++){
+		message->lo |= plaintext[i] << (i * 8);
+	}
+
+	uint32_t shift = 0;
+	for(int i = len/2; i < len; i++){
+		message->hi |= plaintext[i] << (shift * 8);
+		shift++;
+	}
+
+}
+
